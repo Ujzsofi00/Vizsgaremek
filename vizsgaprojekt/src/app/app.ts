7b6bf7efb655bf642568c449918e997cfeed01bd
Ujzsofi00 <93594878+Ujzsofi00@users.noreply.github.com>
2025-11-25 12:05:06 +0100

    login

--- vizsgaprojekt/src/app/app.ts
+++ vizsgaprojekt/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from "./login/login";
+import { Header } from "./header/header";
+import { Register } from "./register/register";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login, Header, Register],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

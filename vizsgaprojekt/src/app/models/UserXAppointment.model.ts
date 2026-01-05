44fb2bdbf683ff8d91a7b8f3548ce55b1cb6c3b0
Ujzsofi00 <93594878+Ujzsofi00@users.noreply.github.com>
2026-01-05 20:18:23 +0100

    infok

--- vizsgaprojekt/src/app/models/UserXAppointment.model.ts
+++ vizsgaprojekt/src/app/models/UserXAppointment.model.ts
@@ -3,3 +3,13 @@ export interface UserXAppointment {
     userId: number;
     appointment: number;
 }
+
+//export class név {
+//   konstruktor               --> Inkább így kéne!
+//}{}
+
+//Dynamic data show(adat a ts fájlból, amit szeretnék megjeleníteni az oldalon) --> Use: ""
+//For example: <img [src]="'assets/users/' + selectedUser.avatar" [alt]="selectedUser.name" />
+//BUT get functionName() is better IN THE TS FILE!
+// Like this: return 'assets/users/' + this.selectedUser.avatar
+//THIS is very important. We have to use it if we are using a property inside of a class.

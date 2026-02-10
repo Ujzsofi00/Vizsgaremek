fa3ea61043b57252425806d7ea42532f85bd4cce
Ujzsofi00 <93594878+Ujzsofi00@users.noreply.github.com>
2026-02-10 12:19:45 +0100

    university-search component

--- vizsgaprojekt/src/app/component/university-search/university-search.ts
+++ vizsgaprojekt/src/app/component/university-search/university-search.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UniversityService } from '../../service/university-service';
+import { university } from '../../models/University.model';
 
 @Component({
   selector: 'app-university-search',
@@ -7,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './university-search.scss',
 })
 export class UniversitySearch {
+  universityService = inject(UniversityService)
+  selectedUniversity: university | null = null;
+  universities: university[] = []
 
+  getUniversityByName(name: string) {
+    if (name != "") {
+      this.universityService.getUniversityByName(name).subscribe({
+        next: response => {
+          this.universities = response
+        }
+      })
+    } else {
+      this.universities = []
+    }
+  }
+
+  selectUniversity(selectedUniversity: university) {
+    this.selectedUniversity = selectedUniversity;
+    this.universities = []
+  }
 }
--- vizsgaprojekt/src/app/service/university-service.ts
+++ vizsgaprojekt/src/app/service/university-service.ts
@@ -1,1 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { university } from '../models/University.model';
+import { Observable } from 'rxjs';
 
+@Injectable({
+  providedIn: 'root',
+})
+export class UniversityService {
+  private http = inject(HttpClient)
+  private baseUrl: string = "http://localhost:8080/university"
+
+  getUniversityByName(name: string): Observable<university[]> {
+    return this.http.get<university[]>(`${this.baseUrl}/${name}`)
+  }
+
+  getAllUniversity(): Observable<university[]> {
+    return this.http.get<university[]>(this.baseUrl)
+  }
+
+  deleteUniversity(id: number) {
+    return this.http.delete(`${this.baseUrl}/${id}`)
+  }
+
+  updateUniversity(updatedUniversity: university): Observable<university> {
+    return this.http.put<university>(this.baseUrl, updatedUniversity)
+  }
+
+  addUniversity(newUniversity: university): Observable<university> {
+    return this.http.post<university>(this.baseUrl, newUniversity)
+  }
+}

deffc0e700aeaff621cf758c698372c8cb3fd690
Ujzsofi00 <93594878+Ujzsofi00@users.noreply.github.com>
2026-02-16 12:18:20 +0100

    Correcting II

--- vizsgaprojekt/src/app/app.config.ts
+++ vizsgaprojekt/src/app/app.config.ts
@@ -3,13 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+import { AuthInterceptor } from './interceptor/AuthInterceptor';
+import { CookieSetterInterceptor } from './interceptor/CookieSetterInterceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch(), withInterceptors([AuthInterceptor, CookieSetterInterceptor])),
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'hu' },
   ]
 };
--- vizsgaprojekt/src/app/app.routes.ts
+++ vizsgaprojekt/src/app/app.routes.ts
@@ -5,15 +5,26 @@ import { AppointmentSelector } from './component/appointment-selector/appointmen
 import { UniversitySearch } from './component/university-search/university-search'; 
 import { AdminPage } from './component/admin-page/admin-page'; 
 import { NotFound } from './component/not-found/not-found';
-import { Register } from './component/register/register'; 
+import { Register } from './component/register/register';
+import { adminAuthGuard } from './component/routeGuard/adminAuthGuard';
+import { Unauthorized } from './component/unauthorized/unauthorized';
+import { UniversityList } from './component/admin-page/university-list/university-list';
+import { UserList } from './component/admin-page/user-list/user-list';
+import { EventManagerPage } from './component/admin-page/event-manager-page/event-manager-page';
+import { AuthGuard } from './component/routeGuard/authGuard';
+import { WorkerAuthGuard } from './component/routeGuard/workerAuthGuard';
 
 export const routes: Routes = [
   { path: "homePage", component: Home, },
   { path: "", pathMatch: "full", redirectTo: "homePage" },
   { path: "login", component: Login },
   { path: "register", component: Register },
-  { path: "appointmentReservation", component: AppointmentSelector },
+  { path: "appointmentReservation", component: AppointmentSelector, canMatch: [AuthGuard] },
   { path: "universitySearch", component: UniversitySearch },
   { path: "adminPage", component: AdminPage },
+  { path: "universities", component: UniversityList, canMatch: [adminAuthGuard] },
+  { path: "users", component: UserList, canMatch: [adminAuthGuard] },
+  { path: "eventManager", component: EventManagerPage, canMatch: [adminAuthGuard, WorkerAuthGuard] },
+  { path: "unauthorized", component: Unauthorized },
   { path: "**", component: NotFound }
 ];
